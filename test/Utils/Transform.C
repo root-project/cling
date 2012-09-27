00f0d19636fb2f734a813a9e4e85a60e00f052c3
Philippe Canal <pcanal@fnal.gov>
2012-09-27 08:49:18 +0000

    Add many more tests ..
    git-svn-id: http://root.cern.ch/svn/root/trunk@46209 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/Utils/Transform.C
+++ test/Utils/Transform.C
@@ -23,6 +23,29 @@ template <typename T, typename U> class C {};
 typedef C<A<B<Double32_t, Int_t> >, Double32_t > CTD;
 typedef C<A<B<const Double32_t, const Int_t> >, Double32_t > CTDConst;
 
+#include <string>
+
+namespace Details {
+   class Impl {};
+}
+
+namespace NS {
+   template <typename T, int size = 0> class ArrayType {};
+   template <typename T> class Array {};
+   
+   template <typename T> class Container {
+   public:
+      class Content {};
+      typedef T Value_t;
+      typedef Content Content_t;
+      typedef ::Details::Impl Impl_t;
+   };
+   
+   template <typename T> class TDataPoint {};
+   typedef TDataPoint<float> TDataPointF;
+   typedef TDataPoint<Double32_t> TDataPointD32;
+}
+
 .rawInput 0
 
 const cling::LookupHelper& lookup = gCling->getLookupHelper();
@@ -67,3 +90,69 @@ lookup.findScope("CTDConst", &t);
 QT = clang::QualType(t, 0);
 Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
 // CHECK: (const char * const) "C<A<B<const Double32_t, const int> >, Double32_t>"
+
+lookup.findScope("std::pair<const std::string,int>", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "std::pair<const std::string, int>"
+
+lookup.findScope("NS::Array<NS::ArrayType<double> >", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "NS::Array<NS::ArrayType<double> >"
+
+lookup.findScope("NS::Array<NS::ArrayType<Double32_t> >", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "NS::Array<NS::ArrayType<Double32_t> >"
+
+lookup.findScope("NS::Container<Long_t>::Content", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "NS::Container<long>::Content"
+
+QT = lookup.findType("NS::Container<Long_t>::Value_t");
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "long"
+
+lookup.findScope("NS::Container<Long_t>::Content_t", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "NS::Container<long>::Content"
+
+lookup.findScope("NS::Container<Long_t>::Impl_t", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "::Details::Impl"
+// Note it should probably return "Details::Impl"
+
+lookup.findScope("NS::Container<Double32_t>::Content", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "NS::Container<Double32_t>::Content"
+
+QT = lookup.findType("NS::Container<Double32_t>::Value_t");
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "double"
+// Really we would want it to say Double32_t but oh well.
+
+lookup.findScope("NS::Container<Double32_t>::Content_t", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "NS::Container<Double32_t>::Content"
+
+lookup.findScope("NS::Container<Double32_t>::Impl_t", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "::Details::Impl"
+// Note it should probably return "Details::Impl"
+
+lookup.findScope("NS::TDataPointF", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "NS::TDataPoint<float>"
+
+lookup.findScope("NS::TDataPointD32", &t);
+QT = clang::QualType(t, 0);
+Transform::GetPartiallyDesugaredType(Ctx, QT, skip).getAsString().c_str()
+// CHECK: (const char * const) "NS::TDataPoint<Double32_t>"
