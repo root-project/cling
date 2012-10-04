a7261215a324675bf1f2fa4198848c72f6036672
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-10-04 15:47:10 +0000

    Add multiplex external sema source, which is a list of sema sources. When it is used it dispatches the events to every element of the list of sources.
    This is used because we want to have ASTDeclReader and our custom external source
    catching LookupUnqualified callbacks, happening on failed lookup.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@46317 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ include/cling/Interpreter/MultiplexExternalSemaSource.h
@@ -0,0 +1,178 @@
+//--------------------------------------------------------------------*- C++ -*-
+// CLING - the C++ LLVM-based InterpreterG :)
+// version: $Id$
+// author:  Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
+//------------------------------------------------------------------------------
+
+#ifndef CLING_MULTIPLEX_EXTERNAL_SEMA_SOURCE_H
+#define CLING_MULTIPLEX_EXTERNAL_SEMA_SOURCE_H
+
+#include "clang/Sema/ExternalSemaSource.h"
+#include "clang/Sema/Weak.h"
+
+#include "llvm/ADT/SmallVector.h"
+
+#include <utility>
+
+namespace clang {
+  class CXXConstructorDecl;
+  class CXXRecordDecl;
+  class DeclaratorDecl;
+  struct ExternalVTableUse;
+  class LookupResult;
+  class NamespaceDecl;
+  class Scope;
+  class Sema;
+  class TypedefNameDecl;
+  class ValueDecl;
+  class VarDecl;
+}
+
+namespace cling {
+
+/// \brief An abstract interface that should be implemented by
+/// external AST sources that also provide information for semantic
+/// analysis.
+class MultiplexExternalSemaSource : public clang::ExternalSemaSource {
+
+private:
+  llvm::SmallVector<ExternalSemaSource*, 4> m_Sources;
+
+public:
+  MultiplexExternalSemaSource() { }
+
+  ~MultiplexExternalSemaSource();
+
+  /// \brief Initialize the semantic source with the Sema instance
+  /// being used to perform semantic analysis on the abstract syntax
+  /// tree.
+  virtual void InitializeSema(clang::Sema& S);
+
+  /// \brief Inform the semantic consumer that Sema is no longer available.
+  virtual void ForgetSema();
+
+  /// \brief Load the contents of the global method pool for a given
+  /// selector.
+  virtual void ReadMethodPool(clang::Selector Sel);
+
+  /// \brief Load the set of namespaces that are known to the external source,
+  /// which will be used during typo correction.
+  virtual void ReadKnownNamespaces(
+                      clang::SmallVectorImpl<clang::NamespaceDecl*>& Namespaces);
+  
+  /// \brief Do last resort, unqualified lookup on a LookupResult that
+  /// Sema cannot find.
+  ///
+  /// \param R a LookupResult that is being recovered.
+  ///
+  /// \param S the Scope of the identifier occurrence.
+  ///
+  /// \return true to tell Sema to recover using the LookupResult.
+  virtual bool LookupUnqualified(clang::LookupResult& R, clang::Scope* S);
+
+  /// \brief Read the set of tentative definitions known to the external Sema
+  /// source.
+  ///
+  /// The external source should append its own tentative definitions to the
+  /// given vector of tentative definitions. Note that this routine may be
+  /// invoked multiple times; the external source should take care not to
+  /// introduce the same declarations repeatedly.
+  virtual void ReadTentativeDefinitions(
+                         clang::SmallVectorImpl<clang::VarDecl*>& TentativeDefs);
+  
+  /// \brief Read the set of unused file-scope declarations known to the
+  /// external Sema source.
+  ///
+  /// The external source should append its own unused, filed-scope to the
+  /// given vector of declarations. Note that this routine may be
+  /// invoked multiple times; the external source should take care not to
+  /// introduce the same declarations repeatedly.
+  virtual void ReadUnusedFileScopedDecls(
+                    clang::SmallVectorImpl<const clang::DeclaratorDecl*>& Decls);
+  
+  /// \brief Read the set of delegating constructors known to the
+  /// external Sema source.
+  ///
+  /// The external source should append its own delegating constructors to the
+  /// given vector of declarations. Note that this routine may be
+  /// invoked multiple times; the external source should take care not to
+  /// introduce the same declarations repeatedly.
+  virtual void ReadDelegatingConstructors(
+                      clang::SmallVectorImpl<clang::CXXConstructorDecl*>& Decls);
+
+  /// \brief Read the set of ext_vector type declarations known to the
+  /// external Sema source.
+  ///
+  /// The external source should append its own ext_vector type declarations to
+  /// the given vector of declarations. Note that this routine may be
+  /// invoked multiple times; the external source should take care not to
+  /// introduce the same declarations repeatedly.
+  virtual void ReadExtVectorDecls(
+                         clang::SmallVectorImpl<clang::TypedefNameDecl*>& Decls);
+
+  /// \brief Read the set of dynamic classes known to the external Sema source.
+  ///
+  /// The external source should append its own dynamic classes to
+  /// the given vector of declarations. Note that this routine may be
+  /// invoked multiple times; the external source should take care not to
+  /// introduce the same declarations repeatedly.
+  virtual void ReadDynamicClasses(
+                           clang::SmallVectorImpl<clang::CXXRecordDecl*>& Decls);
+
+  /// \brief Read the set of locally-scoped external declarations known to the
+  /// external Sema source.
+  ///
+  /// The external source should append its own locally-scoped external
+  /// declarations to the given vector of declarations. Note that this routine 
+  /// may be invoked multiple times; the external source should take care not 
+  /// to introduce the same declarations repeatedly.
+  virtual void ReadLocallyScopedExternalDecls(
+                               clang::SmallVectorImpl<clang::NamedDecl*>& Decls);
+
+  /// \brief Read the set of referenced selectors known to the
+  /// external Sema source.
+  ///
+  /// The external source should append its own referenced selectors to the 
+  /// given vector of selectors. Note that this routine 
+  /// may be invoked multiple times; the external source should take care not 
+  /// to introduce the same selectors repeatedly.
+  virtual void ReadReferencedSelectors(
+                                clang::SmallVectorImpl<std::pair<clang::Selector, 
+                                                 clang::SourceLocation> >& Sels);
+
+  /// \brief Read the set of weak, undeclared identifiers known to the
+  /// external Sema source.
+  ///
+  /// The external source should append its own weak, undeclared identifiers to
+  /// the given vector. Note that this routine may be invoked multiple times; 
+  /// the external source should take care not to introduce the same identifiers
+  /// repeatedly.
+  virtual void ReadWeakUndeclaredIdentifiers(
+                        clang::SmallVectorImpl<std::pair<clang::IdentifierInfo*, 
+                                                         clang::WeakInfo> >& WI);
+
+  /// \brief Read the set of used vtables known to the external Sema source.
+  ///
+  /// The external source should append its own used vtables to the given
+  /// vector. Note that this routine may be invoked multiple times; the external
+  /// source should take care not to introduce the same vtables repeatedly.
+  virtual void ReadUsedVTables(clang::SmallVectorImpl<clang::ExternalVTableUse>& VTables);
+
+  /// \brief Read the set of pending instantiations known to the external
+  /// Sema source.
+  ///
+  /// The external source should append its own pending instantiations to the
+  /// given vector. Note that this routine may be invoked multiple times; the
+  /// external source should take care not to introduce the same instantiations
+  /// repeatedly.
+  virtual void ReadPendingInstantiations(
+                             clang::SmallVectorImpl<std::pair<clang::ValueDecl*, 
+                                              clang::SourceLocation> >& Pending);
+
+  // isa/cast/dyn_cast support
+  static bool classof(const MultiplexExternalSemaSource*) { return true; }
+}; 
+
+} // end namespace clang
+
+#endif // CLING_MULTIPLEX_EXTERNAL_SEMA_SOURCE_H
---
+++ lib/Interpreter/MultiplexExternalSemaSource.cpp
@@ -0,0 +1,105 @@
+//------------------------------------------------------------------------------
+// CLING - the C++ LLVM-based InterpreterG :)
+// version: $Id$
+// author:  Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
+//------------------------------------------------------------------------------
+
+#include "cling/Interpreter/MultiplexExternalSemaSource.h"
+
+#include "clang/Sema/Lookup.h"
+
+using namespace clang;
+
+namespace cling {
+  // pin the vtable here.
+  MultiplexExternalSemaSource::~MultiplexExternalSemaSource() {}
+
+  void MultiplexExternalSemaSource::InitializeSema(Sema& S) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->InitializeSema(S);
+  }
+
+  void MultiplexExternalSemaSource::ForgetSema() {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ForgetSema();
+  }
+
+  void MultiplexExternalSemaSource::ReadMethodPool(Selector Sel) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadMethodPool(Sel);
+  }
+
+  void MultiplexExternalSemaSource::ReadKnownNamespaces(
+                                   SmallVectorImpl<NamespaceDecl*>& Namespaces){
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadKnownNamespaces(Namespaces);
+  }
+  
+  bool MultiplexExternalSemaSource::LookupUnqualified(LookupResult& R, Scope* S){ 
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->LookupUnqualified(R, S);
+    
+    return !R.empty();
+  }
+
+  void MultiplexExternalSemaSource::ReadTentativeDefinitions(
+                                     SmallVectorImpl<VarDecl*>& TentativeDefs) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadTentativeDefinitions(TentativeDefs);
+  }
+  
+  void MultiplexExternalSemaSource::ReadUnusedFileScopedDecls(
+                                SmallVectorImpl<const DeclaratorDecl*>& Decls) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadUnusedFileScopedDecls(Decls);
+  }
+  
+  void MultiplexExternalSemaSource::ReadDelegatingConstructors(
+                                  SmallVectorImpl<CXXConstructorDecl*>& Decls) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadDelegatingConstructors(Decls);
+  }
+
+  void MultiplexExternalSemaSource::ReadExtVectorDecls(
+                                     SmallVectorImpl<TypedefNameDecl*>& Decls) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadExtVectorDecls(Decls);
+  }
+
+  void MultiplexExternalSemaSource::ReadDynamicClasses(
+                                       SmallVectorImpl<CXXRecordDecl*>& Decls) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadDynamicClasses(Decls);
+  }
+
+  void MultiplexExternalSemaSource::ReadLocallyScopedExternalDecls(
+                                           SmallVectorImpl<NamedDecl*>& Decls) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadLocallyScopedExternalDecls(Decls);
+  }
+
+  void MultiplexExternalSemaSource::ReadReferencedSelectors(
+                  SmallVectorImpl<std::pair<Selector, SourceLocation> >& Sels) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadReferencedSelectors(Sels);
+  }
+
+  void MultiplexExternalSemaSource::ReadWeakUndeclaredIdentifiers(
+                   SmallVectorImpl<std::pair<IdentifierInfo*, WeakInfo> >& WI) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadWeakUndeclaredIdentifiers(WI);
+  }
+
+  void MultiplexExternalSemaSource::ReadUsedVTables(
+                                  SmallVectorImpl<ExternalVTableUse>& VTables) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadUsedVTables(VTables);
+  }
+
+  void MultiplexExternalSemaSource::ReadPendingInstantiations(
+                                           SmallVectorImpl<std::pair<ValueDecl*,
+                                                   SourceLocation> >& Pending) {
+    for(size_t i = 0; i < m_Sources.size(); ++i)
+      m_Sources[i]->ReadPendingInstantiations(Pending);
+  }
+} // end namespace cling
