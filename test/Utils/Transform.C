a6a7c6f74863d7388f2d998895265c7943694b31
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-10-09 16:09:42 +0000

    No need of -verify and -I for that test.
    git-svn-id: http://root.cern.ch/svn/root/trunk@46410 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/Utils/Transform.C
+++ test/Utils/Transform.C
@@ -1,4 +1,4 @@
-// RUN: cat %s | %cling -Xclang -verify -I%p | FileCheck %s
+// RUN: cat %s | %cling -I%p | FileCheck %s
 
 // The test verifies the expected behavior in cling::utils::Transform class,
 // which is supposed to provide different transformation of AST nodes and types.
