e8560dc77feac6cb2271579711f84abd9a0a7e33
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-10-18 09:35:38 +0000

    This is definitely expected to fail.
    git-svn-id: http://root.cern.ch/svn/root/trunk@46630 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/Recursion/RecursiveClingInstances.C
+++ test/Recursion/RecursiveClingInstances.C
@@ -1,4 +1,9 @@
-// RUN: cat %s | %cling -I%p
+// RUN: cat %s | %cling -I%p 2>&1
+
+// XFAIL:*
+// XFAIL because there is no such function gCling->createUniqueName() (taking)
+// 0 args. If run in gdb it reports the error. If run with the testsuite it say
+// success. Probably system i/o is not properly piped.
 
 // Tests the ability of cling to host itself. We can have cling instances in
 // cling's runtime. This is important for people, who use cling embedded in
