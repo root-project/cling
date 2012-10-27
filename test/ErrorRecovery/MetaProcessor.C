e5b8645bdaee26cee9594d5ebcfa7f670cca3511
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-10-27 22:20:01 +0000

    Disable test for now. Right now the issue is not that urgent. We will have to revisit and rethink the test case.
    git-svn-id: http://root.cern.ch/svn/root/trunk@46859 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/ErrorRecovery/MetaProcessor.C
+++ test/ErrorRecovery/MetaProcessor.C
@@ -1,14 +1,12 @@
 // RUN: cat %s | %cling -Xclang -verify -I%p
-// XFAIL: vg
+// XFAIL: *
+// The main issue is that expected-error is not propagated to the source file and
+// the expected diagnostics get misplaced.
 .x CannotDotX.h() // expected-error {{use of undeclared identifier 'CannotDotX'}} 
 .x CannotDotX.h() // expected-error {{use of undeclared identifier 'CannotDotX'}}
 
- // Uses a bug in the implementation of .L, which must be fixed soon.
- // Exposes another issue with the VerifyDiagnosticConsumer in the context of 
- // cling. The first .L shouldn't cause errors . However when using the
- // preprocessor most probably we lose track of the errors source locations
- // and files.
-.L CannotDotX.h "// expected-error {{redefinition of 'MyClass'}} expected-error {{expected member name or ';' after declaration specifiers}} expected-node {{previous definition is here}}
+ // Here we cannot revert MyClass from CannotDotX.h
+.L CannotDotX.h // expected-error {{redefinition of 'MyClass'}} expected-error {{expected member name or ';' after declaration specifiers}} expected-node {{previous definition is here}}
 
 
 .L CannotDotX.h
