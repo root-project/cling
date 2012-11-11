660cbfe820764dd158ca5810fd15728d84fd14e1
Axel Naumann <Axel.Naumann@cern.ch>
2012-11-11 12:55:34 +0000

    If the last statement is not an expression, check whether it's a return statement. If it is, remove the implicit-cast-to-void (injected by Sema to heal "return 12" for the original void return type of the wrapper) and adapt the return type of the wrapper to the type of the returned expression. I.e. void wrap() { return (void)12;} will be converted to int wrap() { return 12;}
    Don't search for the WrapperFD, the Transaction has it.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@47166 27541ba8-7e3a-0410-8455-c3a389f83636

--- lib/Interpreter/ReturnSynthesizer.cpp
+++ lib/Interpreter/ReturnSynthesizer.cpp
@@ -28,37 +28,60 @@ namespace cling {
     if (!getTransaction()->getCompilationOpts().ResultEvaluation)
       return;
 
-    for (Transaction::const_iterator iDGR = getTransaction()->decls_begin(), 
-           eDGR = getTransaction()->decls_end(); iDGR != eDGR; ++iDGR)
-      for (DeclGroupRef::const_iterator I = (*iDGR).begin(), E = (*iDGR).end();
-           I != E; ++I)
-        if (FunctionDecl* FD = dyn_cast<FunctionDecl>(*I)) {
-          if (!utils::Analyze::IsWrapper(FD))
-            continue;
+    FunctionDecl* FD = getTransaction()->getWrapperFD();
 
-          int foundAtPos = -1;
-          Expr* lastExpr = utils::Analyze::GetOrCreateLastExpr(FD, &foundAtPos, 
-                                                               /*omitDS*/false,
-                                                               m_Sema);
-          if (lastExpr) {
-            QualType RetTy = lastExpr->getType();
-            if (!RetTy->isVoidType()) {
-              // Change the void function's return type
-              // We can't PushDeclContext, because we don't have scope.
-              Sema::ContextRAII pushedDC(*m_Sema, FD);
-              FunctionProtoType::ExtProtoInfo EPI;
-              QualType FnTy = m_Context->getFunctionType(RetTy,
-                                                         /* ArgArray = */0,
-                                                         /* NumArgs = */0, EPI);
-              FD->setType(FnTy);
-              CompoundStmt* CS = cast<CompoundStmt>(FD->getBody());
-              assert(CS && "Missing body?");
-              // Change it with return stmt (Avoid dealloc/alloc of all el.)
-              *(CS->body_begin() + foundAtPos)
-                = m_Sema->ActOnReturnStmt(lastExpr->getExprLoc(), 
-                                            lastExpr).take();
+    int foundAtPos = -1;
+    Expr* lastExpr = utils::Analyze::GetOrCreateLastExpr(FD, &foundAtPos, 
+                                                         /*omitDS*/false,
+                                                         m_Sema);
+    if (lastExpr) {
+      QualType RetTy = lastExpr->getType();
+      if (!RetTy->isVoidType()) {
+        // Change the void function's return type
+        // We can't PushDeclContext, because we don't have scope.
+        Sema::ContextRAII pushedDC(*m_Sema, FD);
+        FunctionProtoType::ExtProtoInfo EPI;
+        QualType FnTy = m_Context->getFunctionType(RetTy,
+                                                   /* ArgArray = */0,
+                                                   /* NumArgs = */0, EPI);
+        FD->setType(FnTy);
+        CompoundStmt* CS = cast<CompoundStmt>(FD->getBody());
+        assert(CS && "Missing body?");
+        // Change it to a return stmt (Avoid dealloc/alloc of all el.)
+        *(CS->body_begin() + foundAtPos)
+          = m_Sema->ActOnReturnStmt(lastExpr->getExprLoc(), 
+                                    lastExpr).take();
+      }
+    } else if (foundAtPos >= 0) {
+      // check for non-void return statement
+      CompoundStmt* CS = cast<CompoundStmt>(FD->getBody());
+      Stmt* CSS = *(CS->body_begin() + foundAtPos);
+      if (ReturnStmt* RS = dyn_cast<ReturnStmt>(CSS)) {
+        if (Expr* RetV = RS->getRetValue()) {
+          QualType RetTy = RetV->getType();
+          // Any return statement will have been "healed" by Sema
+          // to correspond to the original void return type of the
+          // wrapper, using a ImplicitCastExpr 'void' <ToVoid>.
+          // Remove that.
+          if (RetTy->isVoidType()) {
+            ImplicitCastExpr* VoidCast = dyn_cast<ImplicitCastExpr>(RetV);
+            if (VoidCast) {
+              RS->setRetValue(VoidCast->getSubExpr());
+              RetTy = VoidCast->getSubExpr()->getType();
             }
           }
-        }
+
+          if (!RetTy->isVoidType()) {
+            Sema::ContextRAII pushedDC(*m_Sema, FD);
+            FunctionProtoType::ExtProtoInfo EPI;
+            QualType FnTy = m_Context->getFunctionType(RetTy,
+                                                       /* ArgArray = */0,
+                                                       /* NumArgs = */0,
+                                                       EPI);
+            FD->setType(FnTy);
+          } // not returning void
+        } // have return value
+      } // is a return statement
+    } // have a statement
   }
 } // end namespace cling
