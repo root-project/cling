8cf9ab9121562df7ad04fa3e1e2b6c7f4a472a93
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-11-21 01:15:58 +0000

    Add test case for an item on my TODO list.
    git-svn-id: http://root.cern.ch/svn/root/trunk@47516 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/CodeGeneration/VTables.C
+++ test/CodeGeneration/VTables.C
@@ -1,4 +1,9 @@
 // RUN: cat %s | %cling | FileCheck %s
+// XFAIL:*
+
+// Test whether the interpreter is able to generate properly the symbols 
+// and the vtables of classes.
+
 #include "cling/Interpreter/Interpreter.h"
 #include "cling/Interpreter/LookupHelper.h"
 #include "cling/Utils/AST.h"
@@ -13,17 +18,73 @@ template <typename T> struct C {
 template<typename T> int C<T>::S = 12;
 template class C<int>;
 
+// Nested in function class
+void f() {
+  class NestedC {
+  public:
+    virtual void g() {}
+  };
+}
+
 .rawInput
 
 const cling::LookupHelper& lh = gCling->getLookupHelper();
+clang::Sema& S = gCling->getSema();
 const clang::NamedDecl* D = 0;
 
 clang::DeclContext* DC = 0;
 DC = llvm::dyn_cast_or_null<clang::DeclContext>(const_cast<clang::Decl*>(lh.findScope("C<int>")));
-D = cling::utils::Lookup::Named(&gCling->getSema(), "S", DC);
+D = cling::utils::Lookup::Named(&S, "S", DC);
 gCling->getAddressOfGlobal(D)
 //CHECK-NOT: (void *) 0x0
 
 D = cling::utils::Lookup::Named(&S, "f", DC);
 gCling->getAddressOfGlobal(D)
 //TODO-CHECK-NOT: (void *) 0x0
+
+.rawInput
+// Nested classes
+class N1 {
+public:
+  class N2 {
+  public:
+    class N3 {
+    public:
+      virtual void fN3() {}
+    };
+    virtual void fN2() = 0;
+  };
+};
+.rawInput
+
+DC = llvm::dyn_cast_or_null<clang::DeclContext>(const_cast<clang::Decl*>(lh.findScope("N1::N2")));
+D = cling::utils::Lookup::Named(&S, "fN2", DC);
+gCling->getAddressOfGlobal(D)
+//CHECK-NOT: (void *) 0x0
+
+.rawInput
+
+// vbases
+class V { public: virtual void fV() {} };
+class B1 : virtual public V { /* ... */ };
+class B2 : virtual public V { /* ... */ };
+class B3 : public V { /* ... */ };
+class X : public B1, public B2, public B3 { 
+public: 
+  virtual void fV(){} 
+  struct S {
+    virtual void fS() {}
+  };
+};
+
+.rawInput
+
+DC = llvm::dyn_cast_or_null<clang::DeclContext>(const_cast<clang::Decl*>(lh.findScope("X")));
+D = cling::utils::Lookup::Named(&S, "fV", DC);
+gCling->getAddressOfGlobal(D)
+//CHECK-NOT: (void *) 0x0
+
+DC = llvm::dyn_cast_or_null<clang::DeclContext>(const_cast<clang::Decl*>(lh.findScope("X::S")));
+D = cling::utils::Lookup::Named(&S, "fS", DC);
+gCling->getAddressOfGlobal(D)
+//CHECK-NOT: (void *) 0x0
