60b330abb23276276a5441d3b76af83e2bdc9b5b
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2012-12-01 00:45:43 +0000

    Revert r47746 (which means fully unrevert the r47739). We can run roottest and all seems good. Unbreaks the trunk.
    git-svn-id: http://root.cern.ch/svn/root/trunk@47752 27541ba8-7e3a-0410-8455-c3a389f83636

--- include/cling/MetaProcessor/MetaProcessor.h
+++ include/cling/MetaProcessor/MetaProcessor.h
@@ -23,6 +23,7 @@ namespace cling {
 
   class Interpreter;
   class InputValidator;
+  class MetaParser;
   class StoredValueRef;
 
   class MetaProcessorOpts {
@@ -65,6 +66,10 @@ namespace cling {
     ///
     llvm::OwningPtr<InputValidator> m_InputValidator;
 
+    ///\brief The parser used to parse our tiny "meta" language
+    ///
+    llvm::OwningPtr<MetaParser> m_MetaParser;
+
     ///\brief MetaProcessor's options
     ///
     MetaProcessorOpts m_Options;
@@ -104,14 +109,6 @@ namespace cling {
     bool ProcessMeta(const std::string& input_line,
                      cling::StoredValueRef* result,
                      Interpreter::CompilationResult* compRes = 0);
-
-    ///\brief Shows help for the use of interpreter's meta commands
-    ///
-    void PrintCommandHelp();
-
-    ///\brief Shows statistics about the loaded files
-    ///
-    void PrintFileStats();
 
   public:
     MetaProcessor(Interpreter& interp);
