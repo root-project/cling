03cac10cfd8dd7aa42c0f19bf00bf9326b79e81c
Vassil Vassilev <vasil.georgiev.vasilev@cern.ch>
2013-03-09 20:12:35 +0000

    Now we don't do execute("gCling = (cling::Interpreter*)some_ptr") at startup. This cause the number of unique wrappers to decrease by 1.
    Fix the test correspondingly.
    
    
    git-svn-id: http://root.cern.ch/svn/root/trunk@48854 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/Recursion/RecursiveClingInstances.C
+++ test/Recursion/RecursiveClingInstances.C
@@ -12,5 +12,5 @@ gCling->process("cling::Interpreter *DefaultInterp;");
 gCling->process("DefaultInterp = new cling::Interpreter(1, &argV);");
 gCling->process("DefaultInterp->process(\"#include \\\"cling/Interpreter/Interpreter.h\\\"\");");
 gCling->process("DefaultInterp->process(\"std::string s; gCling->createUniqueName(s); s.c_str()\");");
-// CHECK: (const char *) "__cling_Un1Qu32"
+// CHECK: (const char *) "__cling_Un1Qu31"
 .q
