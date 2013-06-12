65f672f802b39c104700280a7d25028b57e92091
Vassil Vassilev <vvasilev@cern.ch>
2013-06-12 15:07:45 +0200

    Add clarification comment.

--- lib/Interpreter/CIFactory.cpp
+++ lib/Interpreter/CIFactory.cpp
@@ -297,7 +297,7 @@ namespace cling {
   }
 
   void CIFactory::SetClingCustomLangOpts(LangOptions& Opts) {
-    Opts.EmitAllDecls = 0;
+    Opts.EmitAllDecls = 0; // Otherwise if PCH attached will codegen all decls.
     Opts.Exceptions = 1;
     Opts.CXXExceptions = 1;
     Opts.Deprecated = 1;
