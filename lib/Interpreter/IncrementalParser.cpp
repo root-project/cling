1340b29437a56bef17cd66aa4ae1e683147976d4
Vassil Vassilev <vvasilev@cern.ch>
2013-06-14 09:37:45 +0200

    Set the llvm::Module earlier so that the TransactionTransformers could potentially use it.

--- lib/Interpreter/IncrementalParser.cpp
+++ lib/Interpreter/IncrementalParser.cpp
@@ -223,6 +223,8 @@ namespace cling {
           commitTransaction(*I);
     }
 
+    if (hasCodeGenerator())
+      T->setModule(getCodeGenerator()->GetModule());
     // We are sure it's safe to pipe it through the transformers
     bool success = true;
     for (size_t i = 0; i < m_TTransformers.size(); ++i) {
@@ -282,7 +284,6 @@ namespace cling {
       }
 
       getCodeGenerator()->HandleTranslationUnit(getCI()->getASTContext());
-      T->setModule(getCodeGenerator()->GetModule());
 
       // The static initializers might run anything and can thus cause more
       // decls that need to end up in a transaction. But this one is done
