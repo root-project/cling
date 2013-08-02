66514e8d0915b8dbb6fc7fae267359fe14dc648a
Vassil Vassilev <vvasilev@cern.ch>
2013-08-02 17:10:32 +0200

    Add an XFAIL-ing test, as TODO for Baozeng.

---
+++ test/NullDeref/MethodCalls.C
@@ -0,0 +1,15 @@
+// RUN: cat %s | %cling -Xclang -verify
+// This test verifies that we get nice warning if a method on null ptr object is
+// called.
+// XFAIL:*
+extern "C" int printf(const char* fmt, ...);
+class MyClass {
+private:
+  int a;
+public:
+  MyClass() : a(1){}
+  int getA(){return a;}
+};
+MyClass* my = 0;
+my->getA() // expected-warning {{you are about to dereference null ptr, which probably will lead to seg violation. Do you want to proceed?[y/n]}}
+.q
