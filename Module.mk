53a85157fdbf02848a8c203869fc3ac414e87eae
Philippe Canal <pcanal@fnal.gov>
2013-08-08 15:37:38 -0500

    In CLINGCXXFLAGS, put the source include dir first.
    This will make sure that the build is 'consistent' and
    the source version of the cling include files is not
    superseeded by the (possible stale) copy that might
    be in interpreter/llvm/inst.

--- Module.mk
+++ Module.mk
@@ -51,7 +51,7 @@ INCLUDEFILES += $(CLINGDEP)
 # include dir for picking up RuntimeUniverse.h etc - need to
 # 1) copy relevant headers to include/
 # 2) rely on TCling to addIncludePath instead of using CLING_..._INCL below
-CLINGCXXFLAGS = $(patsubst -O%,,$(shell $(LLVMCONFIG) --cxxflags) -I$(CLINGDIR)/include \
+CLINGCXXFLAGS = -I$(CLINGDIR)/include $(patsubst -O%,,$(shell $(LLVMCONFIG) --cxxflags) \
 	-fno-strict-aliasing)
 
 ifeq ($(CTORSINITARRAY),yes)
