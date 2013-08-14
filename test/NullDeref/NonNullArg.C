4f70eafbfd107231874fc58efedcd793b3f85157
Baozeng Ding <sploving1@gmail.com>
2013-08-14 20:36:33 +0200

    Add a simple test testing the new functionality.

---
+++ test/NullDeref/NonNullArg.C
@@ -0,0 +1,9 @@
+// RUN: cat %s | %cling -Xclang -verify
+
+//This file checks a call instruction. The called function has arguments with nonnull attribute.
+#include <string.h>
+char *p;
+strcmp("a", p); // expected-warning {{you are about to dereference null ptr, which probably will lead to seg violation. Do you want to proceed?[y/n]}}
+
+strcmp(p, "a"); // expected-warning {{you are about to dereference null ptr, which probably will lead to seg violation. Do you want to proceed?[y/n]}}
+.q
