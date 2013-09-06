eff9830b09a84b24091c731e42701da7ee320c77
Vassil Vassilev <vvasilev@cern.ch>
2013-09-06 23:18:48 +0200

    Add missing header method declarations.

--- include/cling/Interpreter/RuntimeExceptions.h
+++ include/cling/Interpreter/RuntimeExceptions.h
@@ -6,12 +6,25 @@
 
 #ifndef CLING_RUNTIME_EXCEPTIONS_H
 #define CLING_RUNTIME_EXCEPTIONS_H
+namespace clang {
+  class Sema;
+}
 namespace cling {
   namespace runtime {
     ///\brief Exception that is thrown when a null pointer dereference is found
     /// or a method taking non-null arguments is called with NULL argument.
     /// 
-    class cling_null_deref_exception { };    
+    class cling_null_deref_exception {
+    private:
+      unsigned m_Location;
+      clang::Sema* m_Sema;
+    public:
+      cling_null_deref_exception(void* Loc, clang::Sema* S);
+      ~cling_null_deref_exception();
+
+      void what() throw();
+
+    };    
   } // end namespace runtime
 } // end namespace cling
 #endif // CLING_RUNTIME_EXCEPTIONS_H 
