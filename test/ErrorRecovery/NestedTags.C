559b978279ac8156e0d81729a5e8930dbfc85ae0
Vassil Vassilev <vvasilev@cern.ch>
2013-10-02 12:07:43 +0200

    Fix test semantics.

--- test/ErrorRecovery/NestedTags.C
+++ test/ErrorRecovery/NestedTags.C
@@ -1,11 +1,10 @@
-// RUN: cat %s | %cling -Xclang -verify 2>&1 | FileCheck %s
+// RUN: cat %s | %cling -Xclang -verify 2>&1
 
 // Tests the removal of nested decls 
 
 .storeState "testNestedDecls1"
-struct Outer { struct Inner { enum E{i = 1}; }; };error_here; // expected-error {{error: use of undeclared identifier 'error_here'}}
+struct Outer { struct Inner { enum E{i = 1}; }; };error_here; // expected-error {{use of undeclared identifier 'error_here'}}
 .compareState "testNestedDecls1"
-// CHECK-NOT: File with AST differencies stored in: testNestedDeclsAST1.diff
 
 .rawInput
 namespace Outer { struct Inner { enum E{i = 2}; }; }; 
@@ -14,12 +13,11 @@ namespace Outer { struct Inner { enum E{i = 2}; }; };
 Outer::Inner::i
 // CHECK: (Outer::Inner::E::i) : (int) 2
 
-enum A{a}; // 
+enum A{a};
 
 .storeState "testNestedDecls2"
 enum A{a}; // expected-error {{redefinition of 'A'}} expected-note {{previous definition is here}}
-a // expected-error {{use of undeclared identifier 'a'}}
 .compareState "testNestedDecls2"
-// CHECK-NOT: File with AST differencies stored in: testNestedDeclsAST2.diff
+// CHECK-NOT: Differences
+a // CHECK: (enum A) (A::a) : (int) 0
 .q
- 
