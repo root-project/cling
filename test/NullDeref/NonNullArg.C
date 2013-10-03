84b49183f3f008e6052382bdffdb201cd4a1410c
Axel Naumann <Axel.Naumann@cern.ch>
2013-10-03 09:47:06 +0200

    Adapt to new diag format.

--- test/NullDeref/NonNullArg.C
+++ test/NullDeref/NonNullArg.C
@@ -5,9 +5,9 @@
 // XFAIL: i686-pc-linux-gnu
 
 char *p = 0;
-strcmp("a", p); // expected-warning {{warning: null passed to a callee which requires a non-null argument}}
+strcmp("a", p); // expected-warning {{null passed to a callee which requires a non-null argument}}
 
-strcmp(p, "a"); // expected-warning {{warning: null passed to a callee which requires a non-null argument}}
+strcmp(p, "a"); // expected-warning {{null passed to a callee which requires a non-null argument}}
 
 extern "C" int printf(const char* fmt, ...);
 .rawInput 1
@@ -23,7 +23,7 @@ extern "C" int cannotCallWithNull(int* p) {
   return 1;
 }
 int *q = 0;
-cannotCallWithNull(q); // expected-warning {{warning: null passed to a callee which requires a non-null argument}}
+cannotCallWithNull(q); // expected-warning {{null passed to a callee which requires a non-null argument}}
 //CHECK-NOT: Must not be called with p=0.
 cannotCallWithNull(new int(4))
 //CHECK: (int) 1
