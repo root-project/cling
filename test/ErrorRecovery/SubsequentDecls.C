4de45c1b85d68f0bf0457d53f3f08cc3b6c4a705
Axel Naumann <Axel.Naumann@cern.ch>
2013-10-17 12:29:24 +0200

    Add include path.

--- test/ErrorRecovery/SubsequentDecls.C
+++ test/ErrorRecovery/SubsequentDecls.C
@@ -1,4 +1,4 @@
-// RUN: cat %s | %cling -Xclang -verify 2>&1 | FileCheck %s
+// RUN: cat %s | %cling -I%p -Xclang -verify 2>&1 | FileCheck %s
 
 // Test the removal of decls which are stored in vector of redeclarables 
 .rawInput 1
