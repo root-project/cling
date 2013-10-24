9b352aa82877be653ba908222b0c490ff8b13925
Vassil Vassilev <vvasilev@cern.ch>
2013-10-24 12:03:07 -0500

    Use store/compare state for detailed verification.

--- test/CodeUnloading/Simple.C
+++ test/CodeUnloading/Simple.C
@@ -1,11 +1,14 @@
-// RUN: cat %s | %cling | FileCheck %s
+// RUN: cat %s | %cling 2>&1 | FileCheck %s
 
 // Test the ability of unloading the last transaction. Here as a matter of fact
 // we unload the wrapper as well and TODO: decrement the unique wrapper counter.
+extern "C" int printf(const char* fmt, ...);
+printf("Force printf codegeneration. Otherwise CG will defer it and .storeState will be unhappy.\n");
+//CHECK: Force printf codegeneration. Otherwise CG will defer it and .storeState will be unhappy.
+.storeState "preUnload"
 int f = 0;
 .U
 .rawInput 1
-extern "C" int printf(const char* fmt, ...);
 int f() {
   printf("Now f is a function\n");
   return 0;
@@ -13,5 +16,7 @@ int f() {
 .U
 .rawInput 0
 //CHECK: Now f is a function
+.compareState "preUnload"
+//CHECK-NOT: Differences
 double f = 3.14
 //CHECK: (double) 3.14
