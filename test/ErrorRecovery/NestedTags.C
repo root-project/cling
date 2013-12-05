e054a8126b5643e4e51df8e52a3bfa40590414f5
Vassil Vassilev <vvasilev@cern.ch>
2013-12-05 15:33:19 +0100

    Test enum constant cleanup.

--- test/ErrorRecovery/NestedTags.C
+++ test/ErrorRecovery/NestedTags.C
@@ -1,17 +1,24 @@
 // RUN: cat %s | %cling -Xclang -verify 2>&1 | FileCheck %s
 
 // Tests the removal of nested decls 
-
 .storeState "testNestedDecls1"
 struct Outer { struct Inner { enum E{i = 1}; }; };error_here; // expected-error {{use of undeclared identifier 'error_here'}}
 .compareState "testNestedDecls1"
+// CHECK-NOT: Differences
+
+.rawInput 1
+.storeState "parentLookupTables"
+enum AnEnum {
+   aa = 3,
+   bb = 4,
+ }; error_here; // expected-error {{C++ requires a type specifier for all declarations}}
+.compareState "parentLookupTables"
+.rawInput 0
+// CHECK-NOT: Differences
 
 .rawInput
 namespace Outer { struct Inner { enum E{i = 2}; }; }; 
 .rawInput
-
-Outer::Inner::i
-// CHECK: (Outer::Inner::E::i) : (int) 2
 
 enum A{a};
 
@@ -19,5 +26,9 @@ enum A{a};
 enum A{a}; // expected-error {{redefinition of 'A'}} expected-note {{previous definition is here}}
 .compareState "testNestedDecls2"
 // CHECK-NOT: Differences
+
+Outer::Inner::i
+// CHECK: (Outer::Inner::E::i) : (int) 2
+
 a // CHECK: (enum A) (A::a) : (int) 0
 .q
