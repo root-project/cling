02a9f05fe01493968c25cd688042badec455de1b
Vassil Vassilev <vvasilev@cern.ch>
2014-02-19 14:57:02 +0100

    Follow the change ExecutionContext -> IncrementalExecutor.

--- test/SourceCall/unresolved.C
+++ test/SourceCall/unresolved.C
@@ -18,26 +18,26 @@ extern "C" int functionWithoutDefinition();
 
 int i = 42;
 i = functionWithoutDefinition();
-// CHECK: ExecutionContext: use of undefined symbol 'functionWithoutDefinition'!
-// CHECK: ExecutionContext::executeFunction: symbol 'functionWithoutDefinition' unresolved while linking function 
+// CHECK: IncrementalExecutor: use of undefined symbol 'functionWithoutDefinition'!
+// CHECK: IncrementalExecutor::executeFunction: symbol 'functionWithoutDefinition' unresolved while linking function 
 i = foo();
-// CHECK: ExecutionContext: use of undefined symbol '{{.*}}foo{{.*}}'!
-// CHECK: ExecutionContext::executeFunction: symbol '{{.*}}foo{{.*}}' unresolved while linking function 
+// CHECK: IncrementalExecutor: use of undefined symbol '{{.*}}foo{{.*}}'!
+// CHECK: IncrementalExecutor::executeFunction: symbol '{{.*}}foo{{.*}}' unresolved while linking function 
 
 extern "C" int printf(const char* fmt, ...);
 printf("got i=%d\n", i); // CHECK: got i=42
 int a = 12// CHECK: (int) 12
 
 foo()
-// CHECK: ExecutionContext::executeFunction: symbol '{{.*}}foo{{.*}}' unresolved
+// CHECK: IncrementalExecutor::executeFunction: symbol '{{.*}}foo{{.*}}' unresolved
 functionWithoutDefinition();
-// CHECK: ExecutionContext::executeFunction: symbol 'functionWithoutDefinition' unresolved while linking function 
+// CHECK: IncrementalExecutor::executeFunction: symbol 'functionWithoutDefinition' unresolved while linking function 
 
 bar();
-// CHECK: ExecutionContext: use of undefined symbol '{{.*}}foo{{.*}}'!
-// CHECK: ExecutionContext::executeFunction: symbol '{{.*}}foo{{.*}}' unresolved while linking function 
+// CHECK: IncrementalExecutor: use of undefined symbol '{{.*}}foo{{.*}}'!
+// CHECK: IncrementalExecutor::executeFunction: symbol '{{.*}}foo{{.*}}' unresolved while linking function 
 bar();
-// CHECK: ExecutionContext: calling unresolved symbol, see previous error message!
+// CHECK: IncrementalExecutor: calling unresolved symbol, see previous error message!
 
 i = 13 //CHECK: (int) 13
 .q
