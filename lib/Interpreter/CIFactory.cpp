a172bc0e07f081640ef13757142b54847df0394d
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2014-04-15 10:44:27 +0200

    Fix some of the error messages when starting cling.exe on Windows
    Fixes the following error messages:
    The system cannot find the path specified.
    ERROR in cling::CIFactory::createCI(): cannot extract standard library include paths!
    Invoking:
        echo | LC_ALL=C C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin/cl.exe  /DWIN32 /D_WINDOWS /W3    -xc++ -E -v - 2>&1 >/dev/null | awk '/^#include
     </,/^End of search/{if (!/^#include </ && !/^End of search/){ print }}' | grep -E "(c|g)\+\+"
    results in
    The system cannot find the path specified.
    with exit code 255
    Warning in cling::CIFactory::createCI():
      C++ ABI check not implemented for this standard library
    
    Note there are still a few remaining errors at startup time:
    
    error: cannot mangle RTTI descriptors for type 'exception' yet
    error: cannot mangle the name of type 'exception' into RTTI descriptors yet
    
    Apparently coming from the following statement (at line 284 in Interpreter.cpp, in Interpreter::IncludeCXXRuntime()):
    
        declare("#include \"cling/Interpreter/ValuePrinter.h\"");

--- lib/Interpreter/CIFactory.cpp
+++ lib/Interpreter/CIFactory.cpp
@@ -38,12 +38,246 @@
 #include <ctime>
 #include <cstdio>
 
-#ifdef _WIN32
-#define popen _popen
-#define pclose _pclose
+// Include the necessary headers to interface with the Windows registry and
+// environment.
+#ifdef _MSC_VER
+  #define WIN32_LEAN_AND_MEAN
+  #define NOGDI
+  #define NOMINMAX
+  #include <Windows.h>
+  #define popen _popen
+  #define pclose _pclose
+  #pragma comment(lib, "Advapi32.lib")
 #endif
 
 using namespace clang;
+
+// FIXME: This code has been taken (copied from) llvm/tools/clang/lib/Driver/WindowsToolChain.cpp
+// and should probably go to some platform utils place.
+// the code for VS 11.0 and 12.0 common tools (vs110comntools and vs120comntools) 
+// has been implemented (added) in getVisualStudioDir()
+#ifdef _MSC_VER
+
+/// \brief Read registry string.
+/// This also supports a means to look for high-versioned keys by use
+/// of a $VERSION placeholder in the key path.
+/// $VERSION in the key path is a placeholder for the version number,
+/// causing the highest value path to be searched for and used.
+/// I.e. "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\$VERSION".
+/// There can be additional characters in the component.  Only the numberic
+/// characters are compared.
+static bool getSystemRegistryString(const char *keyPath, const char *valueName,
+                                    char *value, size_t maxLength) {
+  HKEY hRootKey = NULL;
+  HKEY hKey = NULL;
+  const char* subKey = NULL;
+  DWORD valueType;
+  DWORD valueSize = maxLength - 1;
+  long lResult;
+  bool returnValue = false;
+
+  if (strncmp(keyPath, "HKEY_CLASSES_ROOT\\", 18) == 0) {
+    hRootKey = HKEY_CLASSES_ROOT;
+    subKey = keyPath + 18;
+  } else if (strncmp(keyPath, "HKEY_USERS\\", 11) == 0) {
+    hRootKey = HKEY_USERS;
+    subKey = keyPath + 11;
+  } else if (strncmp(keyPath, "HKEY_LOCAL_MACHINE\\", 19) == 0) {
+    hRootKey = HKEY_LOCAL_MACHINE;
+    subKey = keyPath + 19;
+  } else if (strncmp(keyPath, "HKEY_CURRENT_USER\\", 18) == 0) {
+    hRootKey = HKEY_CURRENT_USER;
+    subKey = keyPath + 18;
+  } else {
+    return false;
+  }
+
+  const char *placeHolder = strstr(subKey, "$VERSION");
+  char bestName[256];
+  bestName[0] = '\0';
+  // If we have a $VERSION placeholder, do the highest-version search.
+  if (placeHolder) {
+    const char *keyEnd = placeHolder - 1;
+    const char *nextKey = placeHolder;
+    // Find end of previous key.
+    while ((keyEnd > subKey) && (*keyEnd != '\\'))
+      keyEnd--;
+    // Find end of key containing $VERSION.
+    while (*nextKey && (*nextKey != '\\'))
+      nextKey++;
+    size_t partialKeyLength = keyEnd - subKey;
+    char partialKey[256];
+    if (partialKeyLength > sizeof(partialKey))
+      partialKeyLength = sizeof(partialKey);
+    strncpy(partialKey, subKey, partialKeyLength);
+    partialKey[partialKeyLength] = '\0';
+    HKEY hTopKey = NULL;
+    lResult = RegOpenKeyEx(hRootKey, partialKey, 0, KEY_READ | KEY_WOW64_32KEY,
+                           &hTopKey);
+    if (lResult == ERROR_SUCCESS) {
+      char keyName[256];
+      int bestIndex = -1;
+      double bestValue = 0.0;
+      DWORD index, size = sizeof(keyName) - 1;
+      for (index = 0; RegEnumKeyEx(hTopKey, index, keyName, &size, NULL,
+          NULL, NULL, NULL) == ERROR_SUCCESS; index++) {
+        const char *sp = keyName;
+        while (*sp && !isDigit(*sp))
+          sp++;
+        if (!*sp)
+          continue;
+        const char *ep = sp + 1;
+        while (*ep && (isDigit(*ep) || (*ep == '.')))
+          ep++;
+        char numBuf[32];
+        strncpy(numBuf, sp, sizeof(numBuf) - 1);
+        numBuf[sizeof(numBuf) - 1] = '\0';
+        double dvalue = strtod(numBuf, NULL);
+        if (dvalue > bestValue) {
+          // Test that InstallDir is indeed there before keeping this index.
+          // Open the chosen key path remainder.
+          strcpy(bestName, keyName);
+          // Append rest of key.
+          strncat(bestName, nextKey, sizeof(bestName) - 1);
+          bestName[sizeof(bestName) - 1] = '\0';
+          lResult = RegOpenKeyEx(hTopKey, bestName, 0,
+                                 KEY_READ | KEY_WOW64_32KEY, &hKey);
+          if (lResult == ERROR_SUCCESS) {
+            lResult = RegQueryValueEx(hKey, valueName, NULL, &valueType,
+              (LPBYTE)value, &valueSize);
+            if (lResult == ERROR_SUCCESS) {
+              bestIndex = (int)index;
+              bestValue = dvalue;
+              returnValue = true;
+            }
+            RegCloseKey(hKey);
+          }
+        }
+        size = sizeof(keyName) - 1;
+      }
+      RegCloseKey(hTopKey);
+    }
+  } else {
+    lResult = RegOpenKeyEx(hRootKey, subKey, 0, KEY_READ | KEY_WOW64_32KEY,
+                           &hKey);
+    if (lResult == ERROR_SUCCESS) {
+      lResult = RegQueryValueEx(hKey, valueName, NULL, &valueType,
+        (LPBYTE)value, &valueSize);
+      if (lResult == ERROR_SUCCESS)
+        returnValue = true;
+      RegCloseKey(hKey);
+    }
+  }
+  return returnValue;
+}
+
+/// \brief Get Windows SDK installation directory.
+static bool getWindowsSDKDir(std::string &path) {
+  char windowsSDKInstallDir[256];
+  // Try the Windows registry.
+  bool hasSDKDir = getSystemRegistryString(
+   "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows\\$VERSION",
+                                           "InstallationFolder",
+                                           windowsSDKInstallDir,
+                                           sizeof(windowsSDKInstallDir) - 1);
+    // If we have both vc80 and vc90, pick version we were compiled with.
+  if (hasSDKDir && windowsSDKInstallDir[0]) {
+    path = windowsSDKInstallDir;
+    return true;
+  }
+  return false;
+}
+
+  // Get Visual Studio installation directory.
+static bool getVisualStudioDir(std::string &path) {
+  // First check the environment variables that vsvars32.bat sets.
+  const char* vcinstalldir = getenv("VCINSTALLDIR");
+  if (vcinstalldir) {
+    char *p = const_cast<char *>(strstr(vcinstalldir, "\\VC"));
+    if (p)
+      *p = '\0';
+    path = vcinstalldir;
+    return true;
+  }
+
+  char vsIDEInstallDir[256];
+  char vsExpressIDEInstallDir[256];
+  // Then try the windows registry.
+  bool hasVCDir = getSystemRegistryString(
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\$VERSION",
+    "InstallDir", vsIDEInstallDir, sizeof(vsIDEInstallDir) - 1);
+  bool hasVCExpressDir = getSystemRegistryString(
+    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VCExpress\\$VERSION",
+    "InstallDir", vsExpressIDEInstallDir, sizeof(vsExpressIDEInstallDir) - 1);
+    // If we have both vc80 and vc90, pick version we were compiled with.
+  if (hasVCDir && vsIDEInstallDir[0]) {
+    char *p = (char*)strstr(vsIDEInstallDir, "\\Common7\\IDE");
+    if (p)
+      *p = '\0';
+    path = vsIDEInstallDir;
+    return true;
+  }
+
+  if (hasVCExpressDir && vsExpressIDEInstallDir[0]) {
+    char *p = (char*)strstr(vsExpressIDEInstallDir, "\\Common7\\IDE");
+    if (p)
+      *p = '\0';
+    path = vsExpressIDEInstallDir;
+    return true;
+  }
+
+  // Try the environment.
+  const char *vs120comntools = getenv("VS120COMNTOOLS");
+  const char *vs110comntools = getenv("VS110COMNTOOLS");
+  const char *vs100comntools = getenv("VS100COMNTOOLS");
+  const char *vs90comntools = getenv("VS90COMNTOOLS");
+  const char *vs80comntools = getenv("VS80COMNTOOLS");
+  const char *vscomntools = NULL;
+
+  // Try to find the version that we were compiled with
+  if(false) {}
+  #if (_MSC_VER >= 1800)  // VC120
+  else if(vs120comntools) {
+    vscomntools = vs120comntools;
+  }
+  #elif (_MSC_VER >= 1700)  // VC110
+  else if(vs110comntools) {
+    vscomntools = vs110comntools;
+  }
+  #elif (_MSC_VER >= 1600)  // VC100
+  else if(vs100comntools) {
+    vscomntools = vs100comntools;
+  }
+  #elif (_MSC_VER == 1500) // VC80
+  else if(vs90comntools) {
+    vscomntools = vs90comntools;
+  }
+  #elif (_MSC_VER == 1400) // VC80
+  else if(vs80comntools) {
+    vscomntools = vs80comntools;
+  }
+  #endif
+  // Otherwise find any version we can
+  else if (vs120comntools)
+    vscomntools = vs120comntools;
+  else if (vs110comntools)
+    vscomntools = vs110comntools;
+  else if (vs100comntools)
+    vscomntools = vs100comntools;
+  else if (vs90comntools)
+    vscomntools = vs90comntools;
+  else if (vs80comntools)
+    vscomntools = vs80comntools;
+
+  if (vscomntools && *vscomntools) {
+    const char *p = strstr(vscomntools, "\\Common7\\Tools");
+    path = p ? std::string(vscomntools, p) : vscomntools;
+    return true;
+  }
+  return false;
+}
+
+#endif // _MSC_VER
 
 namespace cling {
   //
@@ -125,6 +359,9 @@ namespace cling {
 #elif _LIBCPP_VERSION
 # define CLING_CXXABIV _LIBCPP_VERSION
 # define CLING_CXXABIS "_LIBCPP_VERSION"
+#elif defined (_MSC_VER)
+# define CLING_CXXABIV _MSC_VER
+# define CLING_CXXABIS "_MSC_VER"
 #else
 # define CLING_CXXABIV -1 // intentionally invalid macro name
 # define CLING_CXXABIS "-1" // intentionally invalid macro name
@@ -133,6 +370,22 @@ namespace cling {
       "C++ ABI check not implemented for this standard library\n";
     return;
 #endif
+#ifdef _MSC_VER
+    HKEY regVS;
+    char subKey[256];
+    int VSVersion = (CLING_CXXABIV / 100) - 6;
+    sprintf(subKey, "VisualStudio.DTE.%d.0", VSVersion);
+    if (RegOpenKeyEx(HKEY_CLASSES_ROOT, subKey, 0, KEY_READ, &regVS) == ERROR_SUCCESS) {
+      RegCloseKey(regVS);
+    }
+    else {
+      llvm::errs()
+        << "Warning in cling::CIFactory::createCI():\n  "
+        "Possible C++ standard library mismatch, compiled with Visual Studio v" 
+        << VSVersion << ".0,\n"
+        "but this version of Visual Studio was not found in your system's registry.\n";
+    }
+#else
     static const char* runABIQuery
       = "echo '#include <vector>' | " LLVM_CXX " -xc++ -dM -E - "
       "| grep 'define " CLING_CXXABIS "' | awk '{print $3}'";
@@ -168,6 +421,7 @@ namespace cling {
       int ExitCode = system(runABIQuery);
       llvm::errs() << "with exit code " << ExitCode << "\n";
     }
+#endif
 #undef CLING_CXXABIV
 #undef CLING_CXXABIS
   }
@@ -178,6 +432,38 @@ namespace cling {
     static std::vector<std::string> HostCXXI;
     if (!IncludesSet) {
       IncludesSet = true;
+#ifdef _MSC_VER
+      // Honor %INCLUDE%. It should know essential search paths with vcvarsall.bat.
+      if (const char *cl_include_dir = getenv("INCLUDE")) {
+        SmallVector<StringRef, 8> Dirs;
+        StringRef(cl_include_dir).split(Dirs, ";");
+        for (SmallVectorImpl<StringRef>::iterator I = Dirs.begin(), E = Dirs.end();
+             I != E; ++I) {
+          StringRef d = *I;
+          if (d.size() == 0)
+            continue;
+          HostCXXI.push_back("-I");
+          HostCXXI.push_back(d);
+        }
+      }
+      std::string VSDir;
+      std::string WindowsSDKDir;
+
+      // When built with access to the proper Windows APIs, try to actually find
+      // the correct include paths first.
+      if (getVisualStudioDir(VSDir)) {
+        HostCXXI.push_back("-I");
+        HostCXXI.push_back(VSDir + "\\VC\\include");
+        if (getWindowsSDKDir(WindowsSDKDir)) {
+          HostCXXI.push_back("-I");
+          HostCXXI.push_back(WindowsSDKDir + "\\include");
+	    }
+        else {
+          HostCXXI.push_back("-I");
+          HostCXXI.push_back(VSDir + "\\VC\\PlatformSDK\\Include");
+	    }
+      }
+#else // _MSC_VER
       static const char *CppInclQuery =
         "echo | LC_ALL=C " LLVM_CXX " -xc++ -E -v - 2>&1 >/dev/null "
         "| awk '/^#include </,/^End of search"
@@ -211,6 +497,7 @@ namespace cling {
         int ExitCode = system(CppInclQuery);
         llvm::errs() << "with exit code " << ExitCode << "\n";
       }
+#endif // _MSC_VER
       CheckABICompatibility();
     }
 
