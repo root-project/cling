6e9cd68cb274d74edc9e23252d6605b4a1f4ca32
Vassil Vassilev <vvasilev@cern.ch>
2014-05-22 15:26:42 +0200

    Construct a library call example. The full reflection-based call still to come.

--- test/LibraryCall/callable_lib.C
+++ test/LibraryCall/callable_lib.C
@@ -8,23 +8,9 @@
 
 // RUN: clang -shared %S/call_lib.c -olibcall_lib%shlibext
 // RUN: cat %s | %cling | FileCheck %s
-// Callable cannot call into libraries yet: XFAIL: *
 
 .L libcall_lib
 extern "C" int cling_testlibrary_function();
 
-#include "cling/Interpreter/Interpreter.h"
-#include "cling/Interpreter/Value.h"
-#include "cling/Interpreter/Callable.h"
-#include "clang/Sema/Sema.h"
-#include "clang/AST/ASTContext.h"
-
-const clang::Decl* TU = gCling->Sema().getASTContext().getTranslationUnitDecl();
-const clang::FunctionDecl* F = gCling->lookupFunctionProto(TU, "cling_testlibrary_function", "");
-assert(F && "cling_testlibrary_function() decl not found");
-cling::Callable Call(*F, *gCling);
-Call.isValid() // CHECK: (_Bool const) true
-std::vector<llvm::GenericValue> ArgVs;
-cling::Value V;
-Call.Invoke(std::vector<llvm::GenericValue>(), &V);
-V // CHECK: (cling::Value) boxes [(int) 66]
+cling_testlibrary_function()
+// CHECK: (int) 66
