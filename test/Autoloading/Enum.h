0a0da4f26d4a86e162662dd8e06c419083fc61ec
manasij7479 <manasij7479@gmail.com>
2014-06-03 03:16:52 +0530

    enum test for template
    (cherry picked from commit ce717aac63c7bd29e836106e255df98b23c4fdb3)

--- test/Autoloading/Enum.h
+++ test/Autoloading/Enum.h
@@ -3,3 +3,12 @@ enum class EC {
   B,
   C
 };
+
+enum E : unsigned int {
+  E_a,
+  E_b,
+  E_c
+};
+
+template<> class Gen<E> {
+};
--- test/Autoloading/EnumTest.C
+++ test/Autoloading/EnumTest.C
@@ -6,13 +6,19 @@
 // LICENSE.TXT for details.
 //------------------------------------------------------------------------------
 
-// RUN: cat %s | %cling -I %S -Xclang -verify
+// RUN: cat %s | %cling 2>&1 -I %S -Xclang -verify
 // Test enumTest
 
-enum class EC;
-EC x=EC::A;
-//expected-error {{}}
+enum __attribute__((annotate("Enum.h"))) class EC;
+EC x=EC::A; 
+// expected-error {{}}
+
+enum E:unsigned int;
+template <typename T> class __attribute__((annotate("Def.h"))) Gen;
+template <> class __attribute__((annotate("Enum.h"))) Gen<E>;
+
+
 #include "Enum.h"
 EC x=EC::A;
-//expected-no-diagnostics
+
 .q
