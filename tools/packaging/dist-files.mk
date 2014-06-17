b029fbb5de11b9bf897996803967b9819db59494
Anirudha Bose <ani07nov@gmail.com>
2014-06-17 08:20:34 +0530

    Hard code cling.exe to be included in the package

--- tools/packaging/dist-files.mk
+++ tools/packaging/dist-files.mk
@@ -18,8 +18,13 @@
 ###############################################################################
 
 BIN_FILES_1 := \
-  cling 
+  cling \
+  cling.exe 
 # CAUTION: The trailing space above is needed. DO NOT delete.
+# TODO: This is currently hard-coded. There must be a platform agnostic way to
+#       guess the extension of binaries (and libraries ?) after compilation,
+#       like in this case .exe. At present there is little need for such a
+#       mechanism since cling.exe is the only case we are dealing with.
 
 BIN_FILES := \
   $(addprefix @prefix@/bin/, $(BIN_FILES_1))
