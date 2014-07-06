dfdffbe5cc287cada0d72502cb8cb689f903b807
Anirudha Bose <ani07nov@gmail.com>
2014-07-06 23:17:04 +0530

    Drop-in replacement of wget for all platforms

--- tools/packaging/cpt.py
+++ tools/packaging/cpt.py
@@ -179,6 +179,32 @@ def box_draw(msg):
 | %s%s|
 +-----------------------------------------------------------------------------+'''%(msg, spacer)
 
+def wget(url):
+    file_name = url.split('/')[-1]
+    print "HTTP request sent, awaiting response... "
+    u = urllib2.urlopen(url)
+    if u.code == 200:
+        print "Connected to %s [200 OK]"%(url)
+    else:
+        exit()
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
+    f.close()
 
 def fetch_llvm():
     box_draw("Fetch source files")
