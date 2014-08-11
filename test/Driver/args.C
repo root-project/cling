f14fbd2b7830b9692bc7fad82a506db5a8a9227f
Vassil Vassilev <vvasilev@cern.ch>
2014-08-11 09:46:23 +0200

    Make sure no diags are emitted.

--- test/Driver/args.C
+++ test/Driver/args.C
@@ -6,9 +6,13 @@
 // LICENSE.TXT for details.
 //------------------------------------------------------------------------------
 
-// RUN: %cling %s "args(42)" | FileCheck %s
+// RUN: %cling %s "args(42)" -Xclang -verify 2>&1 | FileCheck %s
+
+//CHECK-NOT: {{.*error|warning|note:.*}}
+
 extern "C" int printf(const char* fmt, ...);
 void args(int I, const char* S = "ArgString") {
    printf("I=%d\n", I); // CHECK: I=42
    printf("S=%s\n", S); // CHECK: S=ArgString
 }
+// expected-no-diagnostics
