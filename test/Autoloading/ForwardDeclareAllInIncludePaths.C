c0321e417eaf63019e94e9a165d7041bc2a0bb19
Vassil Vassilev <vvasilev@cern.ch>
2014-09-04 10:57:13 +0200

    Test passes.

--- test/Autoloading/ForwardDeclareAllInIncludePaths.C
+++ test/Autoloading/ForwardDeclareAllInIncludePaths.C
@@ -1,6 +1,5 @@
 //RUN: cat %s | %cling -Xclang -verify -DCLING='" %cling "' | FileCheck %s
 //RUN: rm -f /tmp/__cling_fwd_*
-// XFAIL:*
 #include "cling/Interpreter/Interpreter.h"
 
 #include "llvm/ADT/SmallVector.h"
