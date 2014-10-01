19430a0367463ff308c9827e38783a6fd3c7886e
Anirudha Bose <ani07nov@gmail.com>
2014-10-02 00:38:30 +0530

    Convert bytes object to str for Python 3

--- tools/packaging/cpt.py
+++ tools/packaging/cpt.py
@@ -1333,7 +1333,7 @@ prefix = ''
 LLVM_GIT_URL = args['with_llvm_url']
 CLANG_GIT_URL = args['with_clang_url']
 CLING_GIT_URL = args['with_cling_url']
-LLVMRevision = urlopen("https://raw.githubusercontent.com/vgvassilev/cling/master/LastKnownGoodLLVMSVNRevision.txt").readline().strip()
+LLVMRevision = urlopen("https://raw.githubusercontent.com/vgvassilev/cling/master/LastKnownGoodLLVMSVNRevision.txt").readline().strip().decode()
 VERSION = ''
 REVISION = ''
 
