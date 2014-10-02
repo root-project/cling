fd9f5defab251d602fc6596b032089ea2d24e1d1
Axel Naumann <Axel.Naumann@cern.ch>
2014-10-02 15:51:09 +0200

    Mention current failure rate of Sema unloading tests.

--- test/CodeUnloading/RunAgainstClangTestSuite.C
+++ test/CodeUnloading/RunAgainstClangTestSuite.C
@@ -1,2 +1,11 @@
 //RUN: make -C %testexecdir/../../clang/ test TESTSUITE=Sema CLANG=%p/clangTestUnloader.sh
 //XFAIL: *
+
+// Current SemaCXX failures:
+// Expected Passes    : 392
+// Expected Failures  : 1
+// Unexpected Failures: 184
+
+// Current Sema failures:
+// Expected Passes    : 331
+// Unexpected Failures: 90
