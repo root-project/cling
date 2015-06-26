4b08f207938d107c02bb8f03685ae36babde559f
Axel Naumann <Axel.Naumann@cern.ch>
2015-06-26 14:38:21 +0200

    Fix runtime / PCH incompatibility.

--- test/CodeUnloading/PCH/VTables.C
+++ test/CodeUnloading/PCH/VTables.C
@@ -1,5 +1,5 @@
 // RUN: rm -f CompGen.h.pch
-// RUN: clang -x c++-header -fexceptions -fcxx-exceptions -std=c++11 %S/Inputs/CompGen.h -o CompGen.h.pch
+// RUN: clang -x c++-header -fexceptions -fcxx-exceptions -std=c++11 -pthread %S/Inputs/CompGen.h -o CompGen.h.pch
 // RUN: cat %s | %cling -I%p -Xclang -include-pch -Xclang CompGen.h.pch  2>&1 | FileCheck %s
 
 //.storeState "a"
