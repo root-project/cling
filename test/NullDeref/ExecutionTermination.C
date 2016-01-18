0329828fd8598ec8681ac2c1cbeed0bd0fc6de2d
CristinaCristescu <bianca-cristina.cristescu@cern.ch>
2016-01-18 14:56:34 +0100

    Add tests for cling NullDeref.
    If stmt testing and checking that the execution is stopped after
    a null deref warning.

---
+++ test/NullDeref/ExecutionTermination.C
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------
+// CLING - the C++ LLVM-based InterpreterG :)
+//
+// This file is dual-licensed: you can choose to license it under the University
+// of Illinois Open Source License or the GNU Lesser General Public License. See
+// LICENSE.TXT for details.
+//------------------------------------------------------------------------------
+
+// RUN: cat %s | %cling -Xclang -verify
+//This file checks that the execution ends after a null prt dereference.
+
+#include <stdlib.h>
+
+int *p = (int*)0x1;
+*p; exit(1); // expected-warning {{invalid memory pointer passed to a callee:}}
+
+.q
+
