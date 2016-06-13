811be20ec7048c849e307e3064a274529ff90f5c
Axel Naumann <Axel.Naumann@cern.ch>
2016-06-13 13:51:26 +0200

    Fix discovered by additional type safety in clang master.

--- lib/Interpreter/TransactionUnloader.cpp
+++ lib/Interpreter/TransactionUnloader.cpp
@@ -428,7 +428,7 @@ namespace clang {
            iter = iter->getPreviousDecl()) {
         auto declcxx = dyn_cast<CXXRecordDecl>(iter);
         assert(declcxx && "Only CXXRecordDecl have DefinitionData");
-        declcxx->DefinitionData = canon;
+        declcxx->DefinitionData = nullptr;
       }
     }
 
