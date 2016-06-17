7b0276fcae6e638077398f4950b52a01f49831b7
Axel Naumann <Axel.Naumann@cern.ch>
2016-06-17 14:59:17 +0200

    Remove platform-specific code (fails on Darwin), just use cannotCallWithNull.

--- test/NullDeref/NonNullArg.C
+++ test/NullDeref/NonNullArg.C
@@ -8,13 +8,6 @@
 
 // RUN: cat %s | %cling -Xclang -verify | FileCheck %s
 //This file checks a call instruction. The called function has arguments with nonnull attribute.
-#include <string.h>
-
-char *p = 0;
-strcmp("a", p); // expected-warning {{null passed to a callee that requires a non-null argument}}
-
-strcmp(p, "a"); // expected-warning {{null passed to a callee that requires a non-null argument}}
-
 extern "C" int printf(const char* fmt, ...);
 .rawInput 1
 extern "C" int cannotCallWithNull(int* p = 0);
