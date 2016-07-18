f9d19773b8d4387fa4d9cf005ea6531276c16f4c
Frederich Munch <marsupial@users.noreply.github.com>
2016-07-18 01:16:13 -0400

    Fix Token::startToken not initializing length member.

--- lib/MetaProcessor/MetaLexer.h
+++ lib/MetaProcessor/MetaLexer.h
@@ -55,6 +55,7 @@ namespace cling {
       kind = tok::unknown;
       bufStart = Pos;
       value = ~0U;
+      length = 0;
     }
     tok::TokenKind getKind() const { return kind; }
     void setKind(tok::TokenKind K) { kind = K; }
