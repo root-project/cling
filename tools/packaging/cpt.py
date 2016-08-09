d18e9ecdbaf834b90398b7fc2726c3eb2bd1019c
Vassil Vassilev <v.g.vassilev@gmail.com>
2016-08-09 10:22:36 +0200

    We shouldn't force using libcxx.

--- tools/packaging/cpt.py
+++ tools/packaging/cpt.py
@@ -404,7 +404,6 @@ def compile(arg):
 
     build_type = 'Debug' if args.get('create_dev_env') else 'Release'
     cmake_config_flags = (
-            '-DLLVM_ENABLE_LIBCXX=ON '
             '-DCMAKE_BUILD_TYPE={0} '
             '-DLLVM_TARGETS_TO_BUILD=host '
             '-DCMAKE_INSTALL_PREFIX={1} '
