79d830c1227c94a046da576dd204d910f6ae40ba
Bertrand Bellenot <Bertrand.Bellenot@cern.ch>
2016-11-29 15:26:48 +0100

    Test cannot succeed on Windows due to DLL / dyn linker behavior.

--- test/CodeGeneration/Symbols.C
+++ test/CodeGeneration/Symbols.C
@@ -8,6 +8,9 @@
 
 // RUN: clang -shared -fPIC -DBUILD_SHARED %s -o%T/libSymbols%shlibext
 // RUN: %cling --nologo -L%T -lSymbols  %s | FileCheck %s
+// This won't work on Windows as the weak symbol's address is not uniqued
+// across DLL boundaries.
+// REQUIRES: not_system-windows
 
 // Check that weak symbols do not get re-emitted (ROOT-6124)
 extern "C" int printf(const char*,...);
