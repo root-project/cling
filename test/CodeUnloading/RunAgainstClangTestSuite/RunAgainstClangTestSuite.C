b5191075bf5b0a5f34cd3a19ab7b2ae361b3c605
Frederich Munch <marsupial@users.noreply.github.com>
2016-11-30 21:43:18 -0500

    Windows: Get RunAgainstClangTestSuite.C working.

---
+++ test/CodeUnloading/RunAgainstClangTestSuite/RunAgainstClangTestSuite.C
@@ -0,0 +1,11 @@
+// RUN: cd "%testexecdir/../../clang/test" && python "%llvmsrcdir/utils/lit/lit.py" -v -sv --param clang_site_config="%testexecdir/../../clang/test/lit.site.cfg" "%/p/../../../clang/test/Sema/address-constant.c"
+
+// Current SemaCXX failures:
+// Expected Passes    : 392
+// Expected Failures  : 1
+// Unexpected Failures: 184
+
+// Current Sema failures:
+// Expected Passes    : 420
+// Unsupported Tests  : 2
+// Unexpected Failures: 74
