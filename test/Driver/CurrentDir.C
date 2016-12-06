69d727dbbd161d4029011b79f1992f1e841199e5
Frederich Munch <marsupial@users.noreply.github.com>
2016-12-06 13:00:13 -0500

    Revert "Simplify; make Windows compatible (cannot delete cwd)."
    This reverts commit 245922175593ec769be5abf63011d86aa2c02934.

--- test/Driver/CurrentDir.C
+++ test/Driver/CurrentDir.C
@@ -6,10 +6,11 @@
 // LICENSE.TXT for details.
 //------------------------------------------------------------------------------
 
-// Test running a file in the same directory `cling CurrentDir.C`.
-// More info in CIFactory.cpp ("<<< cling interactive line includer >>>")
+// Test runing a file in the same directory `cling CurrentDir.C`
+// More info in CIFactory.cpp createCIImpl (line ~850)
 
-// RUN: cp "%s" "%T/CurrentDir.C" && cd %T && %cling -Xclang -verify CurrentDir.C 2>&1 | FileCheck %s
+// RUN: cd %S && %cling -Xclang -verify CurrentDir.C 2>&1 | FileCheck %s
+// RUN: mkdir %T/Remove && cd %T/Remove && rm -rf %T/Remove && %cling -DTEST_CWDRETURN %s -Xclang -verify 2>&1 | FileCheck --check-prefix CHECK --check-prefix CHECKcwd %s
 // Test testCurrentDir
 
 extern "C" {
@@ -17,7 +18,26 @@ extern "C" {
   char* getcwd(char *buf, std::size_t size);
 }
 
+#ifdef TEST_CWDRETURN
+  // Make sure include still works
+  #include <string.h>
+  #include <vector>
+#endif
+
 void CurrentDir() {
-  printf("Script ran\n"); // CHECK: Script ran
+ #ifdef TEST_CWDRETURN
+    char thisDir[1024];
+    const char *rslt = getcwd(thisDir, sizeof(thisDir));
+    // Make sure cling reported the error
+    // CHECKcwd: Could not get current working directory: {{.*}}
+
+    if (rslt)
+      printf("Working directory exists\n");
+    // CHECK-NOT: Working directory exists
+ #endif
+
+  printf("Script ran\n");
+  // CHECK: Script ran
 }
+
 //expected-no-diagnostics
