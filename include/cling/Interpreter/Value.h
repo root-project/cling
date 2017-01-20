174a183bef41af0eccc34ed061dde31d826ce79d
Axel Naumann <Axel.Naumann@cern.ch>
2017-01-20 08:12:11 +0100

    Implement (faster) Value construction from template type.
    Here, the determination of the storage type is generally trivial.

--- include/cling/Interpreter/Value.h
+++ include/cling/Interpreter/Value.h
@@ -11,6 +11,7 @@
 #define CLING_VALUE_H
 
 #include <stddef.h>
+#include <type_traits>
 
 namespace llvm {
   class raw_ostream;
@@ -80,6 +81,34 @@ namespace cling {
     /// the element of Storage to be used.
     static EStorageType determineStorageType(clang::QualType QT);
 
+    /// \brief Determine the underlying, canonical, desugared, unqualified type:
+    /// the element of Storage to be used.
+    static constexpr EStorageType determineStorageTypeT(...) {
+      return kManagedAllocation;
+    }
+    template <class T, class = typename std::enable_if<std::is_integral<T>::value>::type>
+    static constexpr EStorageType determineStorageTypeT(T*) {
+      if (std::is_signed<T>::value)
+        return kSignedIntegerOrEnumerationType;
+      return kUnsignedIntegerOrEnumerationType;
+    }
+    static constexpr EStorageType determineStorageTypeT(double*) {
+      return kDoubleType;
+    }
+    static constexpr EStorageType determineStorageTypeT(float*) {
+      return kFloatType;
+    }
+    static constexpr EStorageType determineStorageTypeT(long double*) {
+      return kDoubleType;
+    }
+    template <class T>
+    static constexpr EStorageType determineStorageTypeT(T**) {
+      return kPointerType;
+    }
+    static constexpr EStorageType determineStorageTypeT(void*) {
+      return kUnsupportedType;
+    }
+
     /// \brief Allocate storage as needed by the type.
     void ManagedAllocate();
 
@@ -127,6 +156,12 @@ namespace cling {
       }
     };
 
+    Value(void* QualTypeAsOpaquePtr, Interpreter& Interp, EStorageType stType):
+      m_StorageType(stType),
+      m_Type(QualTypeAsOpaquePtr),
+      m_Interpreter(&Interp) {
+    }
+
   public:
     /// \brief Default constructor, creates a value that IsInvalid().
     Value():
@@ -142,12 +177,25 @@ namespace cling {
       other.m_StorageType = kUnsupportedType;
     }
 
-    /// \brief Construct a valid but ininitialized Value. After this call the
+    /// \brief Construct a valid but uninitialized Value. After this call the
     ///   value's storage can be accessed; i.e. calls ManagedAllocate() if
     ///   needed.
     Value(clang::QualType Ty, Interpreter& Interp);
+
     /// \brief Destruct the value; calls ManagedFree() if needed.
     ~Value();
+
+    /// \brief Create a valid but ininitialized Value. After this call the
+    ///   value's storage can be accessed; i.e. calls ManagedAllocate() if
+    ///   needed.
+    template <class T>
+    static Value Create(void* QualTypeAsOpaquePtr, Interpreter& Interp) {
+      EStorageType stType
+        = std::is_reference<T>::value ?
+       determineStorageTypeT((typename std::remove_reference<T>::type**)nullptr)
+        : determineStorageTypeT((T*)nullptr);
+      return Value(QualTypeAsOpaquePtr, Interp, stType);
+    }
 
     Value& operator =(const Value& other);
     Value& operator =(Value&& other);
