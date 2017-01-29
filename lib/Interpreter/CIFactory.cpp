9f62199d73dabad684f062058cf7a7df1b491554
Frederich Munch <marsupial@users.noreply.github.com>
2017-01-28 19:46:30 -0500

    Add ability to control cling output streams colorization. Default to colorize when running in a terminal.

--- lib/Interpreter/CIFactory.cpp
+++ lib/Interpreter/CIFactory.cpp
@@ -775,6 +775,9 @@ static void stringifyPreprocSetting(PreprocessorOptions& PPOpts,
     CI->setInvocation(InvocationPtr.get());
     InvocationPtr.release();
     CI->setDiagnostics(Diags.get()); // Diags is ref-counted
+    if (!OnlyLex)
+      CI->getDiagnosticOpts().ShowColors = cling::utils::ColorizeOutput();
+
 
     // Copied from CompilerInstance::createDiagnostics:
     // Chain in -verify checker, if requested.
