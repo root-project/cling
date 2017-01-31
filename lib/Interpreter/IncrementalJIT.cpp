e7c289878afada7486458dd08a8a60f32db393b8
Philippe Canal <pcanal@fnal.gov>
2017-01-31 12:22:58 -0600

    Enable GDB listener / obj registration.
    This allows debuggers to see the actual content of the JIT part of a stack trace.
    
    This revert per se 9428c1f1907b3f01208dcc6095c5707011621310

--- lib/Interpreter/IncrementalJIT.cpp
+++ lib/Interpreter/IncrementalJIT.cpp
@@ -161,6 +161,7 @@ IncrementalJIT::IncrementalJIT(IncrementalExecutor& exe,
 
   // Make debug symbols available.
   m_GDBListener = 0; // JITEventListener::createGDBRegistrationListener();
+  m_GDBListener = JITEventListener::createGDBRegistrationListener();
 
 // #if MCJIT
 //   llvm::EngineBuilder builder(std::move(m));
