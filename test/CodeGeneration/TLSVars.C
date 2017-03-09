5c4de74ccbdf5bd88b592d4961ee05e693fb1879
Frederich Munch <marsupial@users.noreply.github.com>
2017-03-08 22:29:38 -0500

    Windows: Return current thread’s _Init_thread_epoch from __emutls_get_address. Fixes function level static variables.

--- test/CodeGeneration/TLSVars.C
+++ test/CodeGeneration/TLSVars.C
@@ -6,7 +6,7 @@
 // LICENSE.TXT for details.
 //------------------------------------------------------------------------------
 
-// RUN: cat %s | %cling -Xclang -verify | FileCheck %s
+// RUN: cat %s | %cling -Xclang -verify 2>&1 | FileCheck %s
 
 // Test whether the TLS data is properly handled
 
@@ -17,10 +17,10 @@
 #include <stdlib.h>
 
 thread_local unsigned int TLSCounter = 1;
-std::mutex WriteMutex;
+// std::mutex WriteMutex;
 
 static void WriteValue(const char* Name, unsigned Val) {
-  std::lock_guard<std::mutex> Lock(WriteMutex);
+  // std::lock_guard<std::mutex> Lock(WriteMutex);
   printf("TLSCounter for '%s' : %u\n", Name, Val);
 }
  
@@ -45,10 +45,36 @@ WriteValue("main", TLSCounter);
 
 
 // CHECK: TLSCounter for 'A' : 2
-// CHECK: TLSCounter for 'B' : 2
-// CHECK: TLSCounter for '10' : 12
-// CHECK: TLSCounter for '5' : 7
-// CHECK: TLSCounter for 'main' : 1
+// CHECK-NEXT: TLSCounter for 'B' : 2
+// CHECK-NEXT: TLSCounter for '10' : 12
+// CHECK-NEXT: TLSCounter for '5' : 7
+// CHECK-NEXT: TLSCounter for 'main' : 1
+
+// Below is predominately for Windows
+struct TERD {
+    const char *Name;
+    TERD(const char *N) : Name(N) { printf("TERD::TERD::%s\n", Name); }
+    ~TERD() { printf("TERD::~TERD::%s\n", Name); }
+};
+static TERD& inst01() {
+    static TERD st01("inst01");
+    return st01;
+}
+static TERD& inst02() {
+    static TERD st02("inst02");
+    return st02;
+}
+
+inst01();
+// CHECK-NEXT: TERD::TERD::inst01
+std::thread(&inst01).join();
+
+std::thread(&inst02).join();
+// CHECK-NEXT: TERD::TERD::inst02
+inst02();
 
 // expected-no-diagnostics
 .q
+
+// CHECK-NEXT: TERD::~TERD::inst02
+// CHECK-NEXT: TERD::~TERD::inst01
