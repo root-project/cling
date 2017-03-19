e2c39705ad4401615e63154c12138aa35e00cf74
Axel Naumann <Axel.Naumann@cern.ch>
2017-03-19 21:35:04 +0100

    Use llvm and clang of cling.

--- cmake/modules/ClingConfig.cmake
+++ cmake/modules/ClingConfig.cmake
@@ -4,7 +4,9 @@
 # uses LLVM's. When it does, we should move this file to ClingConfig.cmake.in
 # and call configure_file() on it.
 
-find_package(LLVM REQUIRED CONFIG)
+# Don't just use any llvm / clang: cling needs its own:
+find_package(LLVM REQUIRED CONFIG NO_DEFAULT_PATH PATHS "${CMAKE_CURRENT_LIST_DIR}/../llvm")
+find_package(clang REQUIRED CONFIG NO_DEFAULT_PATH PATHS "${CMAKE_CURRENT_LIST_DIR}/../clang")
 
 # Provide all our library targets to users.
 include("${CMAKE_CURRENT_LIST_DIR}/ClingTargets.cmake")
