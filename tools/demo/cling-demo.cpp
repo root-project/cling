e847f2f5bc7b41dd1da19b4d9ebfec64164312b9
Axel Naumann <Axel.Naumann@cern.ch>
2017-03-19 23:03:53 +0100

    Add (c) header.

--- tools/demo/cling-demo.cpp
+++ tools/demo/cling-demo.cpp
@@ -1,3 +1,12 @@
+//------------------------------------------------------------------------------
+// CLING - the C++ LLVM-based InterpreterG :)
+// author:  Axel Naumann <axel@cern.ch>
+//
+// This file is dual-licensed: you can choose to license it under the University
+// of Illinois Open Source License or the GNU Lesser General Public License. See
+// LICENSE.TXT for details.
+//------------------------------------------------------------------------------
+
 #include <cling/Interpreter/Interpreter.h>
 #include <cling/Interpreter/Value.h>
 #include <iostream>
