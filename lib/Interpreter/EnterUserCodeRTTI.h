5a7c0d122cef0e9fa35b5ce049a98073a5642bda
Axel Naumann <Axel.Naumann@cern.ch>
2017-09-15 12:04:05 +0200

    Add RAII for LockCompilationDuringUserCodeExecution.

--- lib/Interpreter/EnterUserCodeRTTI.h
+++ lib/Interpreter/EnterUserCodeRTTI.h
@@ -16,8 +16,10 @@
 namespace cling {
 ///\brief Unlocks and then upon destruction locks the interpreter again.
 struct EnterUserCodeRTTI {
-  InterpreterCallbacks* fCallbacks; // callbacks used to un/lock.
-  void* fStateInfo = nullptr;       // info provided to ReturnedFromUserCode().
+  /// Callbacks used to un/lock.
+  InterpreterCallbacks* fCallbacks;
+  /// Info provided to ReturnedFromUserCode().
+  void* fStateInfo = nullptr;
   EnterUserCodeRTTI(InterpreterCallbacks* callbacks): fCallbacks(callbacks) {
     if (fCallbacks)
       fStateInfo = fCallbacks->EnteringUserCode();
@@ -29,6 +31,26 @@ struct EnterUserCodeRTTI {
   ~EnterUserCodeRTTI() {
     if (fCallbacks)
       fCallbacks->ReturnedFromUserCode(fStateInfo);
+  }
+};
+
+struct LockCompilationDuringUserCodeExecutionRAII {
+  /// Callbacks used to un/lock.
+  InterpreterCallbacks* fCallbacks;
+  /// Info provided to UnlockCompilationDuringUserCodeExecution().
+  void* fStateInfo = nullptr;
+  LockCompilationDuringUserCodeExecutionRAII(InterpreterCallbacks* callbacks):
+  fCallbacks(callbacks) {
+    if (fCallbacks)
+      fStateInfo = fCallbacks->LockCompilationDuringUserCodeExecution();
+  }
+
+  LockCompilationDuringUserCodeExecutionRAII(Interpreter& interp):
+  LockCompilationDuringUserCodeExecutionRAII(interp.getCallbacks()) {}
+
+  ~LockCompilationDuringUserCodeExecutionRAII() {
+    if (fCallbacks)
+      fCallbacks->UnlockCompilationDuringUserCodeExecution(fStateInfo);
   }
 };
 }
