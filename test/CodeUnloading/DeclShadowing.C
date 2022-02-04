acfb99818878eeb02687790874adad3147df7ef7
Javier Lopez-Gomez <javier.lopez.gomez@cern.ch>
2022-02-04 15:14:13 +0100

    Improve DeclShadowing.C test
    Check also the declaration context, which should be an inline namespace if the
    DefinitionShadower is enabled or the TU otherwise.

--- test/CodeUnloading/DeclShadowing.C
+++ test/CodeUnloading/DeclShadowing.C
@@ -7,11 +7,21 @@
 //------------------------------------------------------------------------------
 
 // RUN: cat %s | %cling 2>&1 | FileCheck %s
+#include "cling/Interpreter/Interpreter.h"
+#include "cling/Utils/AST.h"
+#include "clang/AST/Decl.h"
+
 #include <type_traits>
 #include <cstdlib>
 #include <string>
 
+unsigned _i;
+struct _X {};
+int _g() { return 1; }
+
 cling::runtime::gClingOpts->AllowRedefinition = 1;
+
+void g() {}
 
 // ==== Test UsingDirectiveDecl/UsingDecl
 // These should not be nested into a `__cling_N5xxx' namespace (but placed at
@@ -87,5 +97,44 @@ f(33)
 //CHECK-NEXT: (int) 43605
 f(3.3f)
 //CHECK-NEXT: (int) 21930
+
+cling::runtime::gClingOpts->AllowRedefinition = 0;
+
+// ==== Check DeclContext
+// If DefinitionShadower is enabled, declaration context is an inline namespace;
+// otherwise it should be the TU.
+clang::Sema& _S = gCling->getSema();
+const clang::NamedDecl *_decl{nullptr};
+
+_decl = cling::utils::Lookup::Named(&_S, "_i", nullptr)
+//CHECK-NEXT: (const clang::NamedDecl *) 0x{{[1-9a-f][0-9a-f]*$}}
+_decl->getDeclContext()->isTranslationUnit()
+//CHECK-NEXT: (bool) true
+
+_decl = cling::utils::Lookup::Named(&_S, "_X", nullptr)
+//CHECK-NEXT: (const clang::NamedDecl *) 0x{{[1-9a-f][0-9a-f]*$}}
+_decl->getDeclContext()->isTranslationUnit()
+//CHECK-NEXT: (bool) true
+
+_decl = cling::utils::Lookup::Named(&_S, "_g", nullptr)
+//CHECK-NEXT: (const clang::NamedDecl *) 0x{{[1-9a-f][0-9a-f]*$}}
+_decl->getDeclContext()->isTranslationUnit()
+//CHECK-NEXT: (bool) true
+
+_decl = cling::utils::Lookup::Named(&_S, "bar", nullptr)
+//CHECK-NEXT: (const clang::NamedDecl *) 0x{{[1-9a-f][0-9a-f]*$}}
+_decl->getDeclContext()->isInlineNamespace()
+//CHECK-NEXT: (bool) true
+
+_decl = cling::utils::Lookup::Named(&_S, "MyStruct", nullptr)
+//CHECK-NEXT: (const clang::NamedDecl *) 0x{{[1-9a-f][0-9a-f]*$}}
+_decl->getDeclContext()->isInlineNamespace()
+//CHECK-NEXT: (bool) true
+
+_decl = cling::utils::Lookup::Named(&_S, "g", nullptr)
+//CHECK-NEXT: (const clang::NamedDecl *) 0x{{[1-9a-f][0-9a-f]*$}}
+_decl->getDeclContext()->isInlineNamespace()
+//CHECK-NEXT: (bool) true
+
 //expected-no-diagnostics
 .q
