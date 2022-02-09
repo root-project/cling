f2520d9a39f1d30d1d8dcd81d7f1d3663c2ad472
Vassil Vassilev <v.g.vassilev@gmail.com>
2022-02-09 20:15:30 +0000

    PrintHelp is now lower case.

--- lib/Interpreter/InvocationOptions.cpp
+++ lib/Interpreter/InvocationOptions.cpp
@@ -236,12 +236,12 @@ InvocationOptions::InvocationOptions(int argc, const char* const* argv) :
 void InvocationOptions::PrintHelp() {
   std::unique_ptr<OptTable> Opts(CreateClingOptTable());
 
-  Opts->PrintHelp(cling::outs(), "cling",
+  Opts->printHelp(cling::outs(), "cling",
                   "cling: LLVM/clang C++ Interpreter: http://cern.ch/cling");
 
   cling::outs() << "\n\n";
 
   const OptTable& OptsC1 = getDriverOptTable();
-  OptsC1.PrintHelp(cling::outs(), "clang -cc1",
+  OptsC1.printHelp(cling::outs(), "clang -cc1",
                    "LLVM 'Clang' Compiler: http://clang.llvm.org");
 }
