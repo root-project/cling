82da1c0ad81ae444f4d3af6ba6d4f24ed85c9d7a
m0dulev1 <73461919+m0dulev1@users.noreply.github.com>
2022-02-08 17:45:19 -0800

    Delete www directory

--- www/scripts/cling.js
+++
@@ -1,6 +0,0 @@
-requirejs(["//cdn.jsdelivr.net/highlight.js/9.2.0/highlight.min.js"], function() {
-  hljs.initHighlightingOnLoad();
-});
-function selectMenu(tab) {
-  document.getElementById('iheader').contentDocument.getElementById('tab_' + tab).className='selected'
-}
