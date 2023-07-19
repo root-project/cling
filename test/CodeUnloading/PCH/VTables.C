c1574685cb74ea67acc8b9a650c2d4dafc19c237
Jonas Hahnfeld <jonas.hahnfeld@cern.ch>
2023-07-19 15:37:44 +0200

    Automatically adjust C++ version of PCH in Cling's test
    This fixes test/CodeUnloading/PCH/VTables.C with C++17 and later.

--- test/CodeUnloading/PCH/VTables.C
+++ test/CodeUnloading/PCH/VTables.C
@@ -1,7 +1,7 @@
 // RUN: %mkdir "%T/Rel/Path" || true
 // RUN: %rm "CompGen.h.pch" && %rm "%T/Rel/Path/Relative.pch"
-// RUN: clang -x c++-header -fexceptions -fcxx-exceptions -std=c++14 -pthread %S/Inputs/CompGen.h -o CompGen.h.pch
-// RUN: clang -x c++-header -fexceptions -fcxx-exceptions -std=c++14 -pthread %S/Inputs/CompGen.h -o %T/Rel/Path/Relative.pch
+// RUN: clang -x c++-header -fexceptions -fcxx-exceptions -std=%std_cxx -pthread %S/Inputs/CompGen.h -o CompGen.h.pch
+// RUN: clang -x c++-header -fexceptions -fcxx-exceptions -std=%std_cxx -pthread %S/Inputs/CompGen.h -o %T/Rel/Path/Relative.pch
 // RUN: cat %s | %cling -I%p -Xclang -include-pch -Xclang CompGen.h.pch  2>&1 | FileCheck %s
 // RUN: cat %s | %cling -I%p -I%T/Rel/Path -include-pch Relative.pch 2>&1 | FileCheck %s
 
