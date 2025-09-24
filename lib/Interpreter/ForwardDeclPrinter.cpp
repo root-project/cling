58cdb28b1fcdc9a876ffed55d8b4836ef5aa0e29
Devajith Valaparambil Sreeramaswamy <devajith.valaparambil.sreeramaswamy@cern.ch>
2025-09-24 19:34:40 +0200

    Use TemplateArgumentLoc for type-param DefaultArgument
    Ref: https://github.com/llvm/llvm-project/commit/e42b799bb28815431f2c5a95f7e13fde3f1b36a1

--- lib/Interpreter/ForwardDeclPrinter.cpp
+++ lib/Interpreter/ForwardDeclPrinter.cpp
@@ -938,7 +938,7 @@ namespace cling {
            ArgQT = Args->get(i).getAsType();
         }
         else if (TTP->hasDefaultArgument()) {
-           ArgQT = TTP->getDefaultArgument();
+           ArgQT = TTP->getDefaultArgument().getArgument().getAsType();
         }
         if (!ArgQT.isNull()) {
           QualType ArgFQQT
@@ -964,7 +964,7 @@ namespace cling {
           Args->get(i).print(m_Policy, Stream, /*IncludeType=*/true);
         }
         else if (NTTP->hasDefaultArgument()) {
-          Expr* DefArg = NTTP->getDefaultArgument()->IgnoreImpCasts();
+          Expr* DefArg = NTTP->getDefaultArgument().getArgument().getAsExpr()->IgnoreImpCasts();
           if (DeclRefExpr* DRE = dyn_cast<DeclRefExpr>(DefArg)) {
             Visit(DRE->getFoundDecl());
             if (m_SkipFlag) {
